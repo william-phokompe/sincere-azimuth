bbde804de52b75d73feda75d3c39946bdd0fae8c
William Phokompe <phokompe.william@outlook.com>
2019-09-18 21:10:10 +0200

    Create a Book model, (a struct in cases of Go)

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 )
 
 // Book structures (Basically a model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
 
 func main() {
 	// Initialise router
