406ecd416c0a1f4389220958dcfbcb7c36c8892c
William Phokompe <phokompe.william@outlook.com>
2019-09-18 19:22:05 +0200

    Create all the routes in main and create a server listening on port 8000

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+// Book structures (Basically a model)
+
 func main() {
+	// Initialise router
 	r := mux.NewRouter()
+
+	// Route Handlers / Enpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
